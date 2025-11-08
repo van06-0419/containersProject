4786280b4528edba653e7112cbf25b79d785e62d
van06-0419 <zhiyuan.wang@yandex.ru>
2025-11-08 14:29:12 +0700

    Update ListContainer.h

--- include/ListContainer.h
+++ include/ListContainer.h
@@ -2,113 +2,131 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename T>
 class ListContainer {
 private:
-    struct Node {
-        T data;
-        Node* prev;
-        Node* next;
-        Node(const T& val) : data(val), prev(nullptr), next(nullptr) {}
-    };
+	struct Node {
+		int data;
+		Node* prev;
+		Node* next;
+		Node(const int& val):data(val),prev(nullptr),next(nullptr){}
+	};
 
-    Node* head;
-    Node* tail;
-    size_t m_size;
+	Node* head;
+	Node* tail;
+	size_t m_size;
 
 public:
-    ListContainer() : head(nullptr), tail(nullptr), m_size(0) {}
-    ~ListContainer() {
-        Node* current = head;
-        while (current) {
-            Node* temp = current->next;
-            delete current;
-            current = temp;
-        }
-    }
+	ListContainer() : head(nullptr),tail(nullptr),m_size(0){}
+	~ListContainer() {
+		Node* current = head;
+		while (current) {
+			Node* temp = current->next;
+			delete current;
+			current = temp;
+		}
+	}
 
-    ListContainer(ListContainer&& other) noexcept
-        : head(other.head), tail(other.tail), m_size(other.m_size) {
-        other.head = other.tail = nullptr;
-        other.m_size = 0;
-    }
+	void push_back(const int& value) {
+		Node* newNode = new Node(value);
+		if (head == nullptr) head = tail = newNode;
+		else {
+			tail->next = newNode;
+			newNode->prev = tail;
+			tail = newNode;
+		}
+		++m_size;
+	}
 
-    ListContainer& operator=(ListContainer&& other) noexcept {
-        if (this != &other) {
-            this->~ListContainer();
-            head = other.head;
-            tail = other.tail;
-            m_size = other.m_size;
-            other.head = other.tail = nullptr;
-            other.m_size = 0;
-        }
-        return *this;
-    }
+	void insert(size_t pos, const int& value) {
+		if (pos > m_size)
+			throw std::out_of_range("pos is outside of current list size");
 
-    void push_back(const T& value) {
-        Node* newNode = new Node(value);
-        if (!head) head = tail = newNode;
-        else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-        ++m_size;
-    }
+		if (pos == m_size) {
+			push_back(value);
+			return;
+		}
 
-    void insert(size_t pos, const T& value) {
-        if (pos > m_size) throw std::out_of_range("Invalid index");
-        if (pos == m_size) { push_back(value); return; }
+		if (pos == 0) {
+			Node* newNode = new Node(value);
+			newNode->next = head;
+			if (head != nullptr) head->prev = newNode;
+			else tail = newNode;
+			head = newNode;
+			++m_size;
+			return;
+		}
 
-        Node* newNode = new Node(value);
-        Node* current = head;
-        for (size_t i = 0; i < pos; ++i)
-            current = current->next;
+		Node* newNode = new Node(value);
+		Node* current = head;
+		for (size_t i = 0; i < pos; i++)
+			current = current->next;
 
-        newNode->next = current;
-        newNode->prev = current->prev;
-        if (current->prev) current->prev->next = newNode;
-        else head = newNode;
-        current->prev = newNode;
-        ++m_size;
-    }
+		newNode->next = current;
+		newNode->prev = current->prev;
+		current->prev->next = newNode;
+		current->prev = newNode;
 
-    void erase(size_t pos) {
-        if (pos >= m_size) throw std::out_of_range("Invalid index");
-        Node* current = head;
-        for (size_t i = 0; i < pos; ++i)
-            current = current->next;
+		++m_size;
+	}
 
-        if (current->prev) current->prev->next = current->next;
-        else head = current->next;
-        if (current->next) current->next->prev = current->prev;
-        else tail = current->prev;
+	void erase(size_t pos) {
+		if (pos > m_size)
+			throw std::out_of_range("pos is outside of current list size");
 
-        delete current;
-        --m_size;
-    }
+		if (pos == 0) {
+			Node* temp = head;
+			head = head->next;
+			if (head != nullptr) {
+				head->prev = nullptr;
+			}
+			else tail = nullptr;
+			delete temp;
+			--m_size;
+			return;
+		}
 
-    size_t size() const { return m_size; }
+		if (pos == m_size - 1) {
+			Node* temp = tail;
+			tail = tail->prev;
+			tail->next = nullptr;
+			delete temp;
+			--m_size;
+			return;
+		}
 
-    void print() const {
-        Node* current = head;
-        while (current) {
-            std::cout << current->data;
-            if (current->next) std::cout << ", ";
-            current = current->next;
-        }
-        std::cout << std::endl;
-    }
+		Node* current = head;
+		for (size_t i = 0; i < pos; i++) 
+			current = current->next;
 
-    class Iterator {
-        Node* node;
-    public:
-        Iterator(Node* n) : node(n) {}
-        T& operator*() const { return node->data; }
-        Iterator& operator++() { node = node->next; return *this; }
-        bool operator!=(const Iterator& other) const { return node != other.node; }
-    };
+		current->prev->next = current->next;
+		current->next->prev = current->prev;
 
-    Iterator begin() const { return Iterator(head); }
-    Iterator end() const { return Iterator(nullptr); }
+		delete current;
+		--m_size;
+	}
+
+	size_t size() const { return m_size; }
+
+	void print() const {
+		Node* current = head;
+		while (current != nullptr) {
+			std::cout << current->data;
+			if (current->next != nullptr) std::cout << ",";
+			current = current->next;
+		}
+		std::cout << std::endl;
+	}
+
+	class Iterator {
+		Node* node;
+	public:
+		Iterator(Node* n) : node(n) {}
+		int& operator*() const { return node->data; }
+		Iterator& operator++() { node = node->next; return *this; }
+		bool operator!=(const Iterator& other) const { return node != other.node; }
+	};
+
+	Iterator begin() const { return Iterator(head); }
+	Iterator end() const { return Iterator(nullptr); }
 };
+
