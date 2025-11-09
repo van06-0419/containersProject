7597fa6d28abad4e8ef47511a9fd84ded63208ed
van06-0419 <zhiyuan.wang@yandex.ru>
2025-11-09 19:53:42 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,6 @@
 using namespace std;
 
 int main() {
-    cout << "Тест последовательного контейнера" << endl;
-    SeqContainer seq;
-    for (int i = 0; i < 10; ++i) seq.push_back(i);
-    seq.print();
-    cout << "Размер: " << seq.size() << endl;
-
-    seq.erase(2);
-    seq.erase(3);
-    seq.erase(4);
-    seq.print();
-
-    seq.insert(0, 10);
-    seq.print();
-
-    seq.insert(seq.size() / 2, 20);
-    seq.print();
-
-    seq.push_back(30);
-    seq.print();
-
-    cout << endl << "Тест спискового контейнера" << endl;
-    ListContainer list;
-    for (int i = 0; i < 10; ++i) list.push_back(i);
-    list.print();
-    cout << "Размер: " << list.size() << endl;
-
-    list.erase(2);
-    list.erase(3);
-    list.erase(4);
-    list.print();
-
-    list.insert(0, 10);
-    list.print();
-
-    list.insert(list.size() / 2, 20);
-    list.print();
-
-    list.push_back(30);
-    list.print();
-
     cout << endl << "Тест односвязного списка" << endl;
     SinglyListContainer slist;
     for (int i = 0; i < 10; ++i) slist.push_back(i);
@@ -64,6 +24,46 @@ int main() {
     
     slist.push_back(30);
     slist.print();
+
+    cout << endl << "Тест двусвязного списка" << endl;
+    ListContainer list;
+    for (int i = 0; i < 10; ++i) list.push_back(i);
+    list.print();
+    cout << "Размер: " << list.size() << endl;
+
+    list.erase(2);
+    list.erase(3);
+    list.erase(4);
+    list.print();
+
+    list.insert(0, 10);
+    list.print();
+
+    list.insert(list.size() / 2, 20);
+    list.print();
+
+    list.push_back(30);
+    list.print();
+
+    cout << "Тест последовательного контейнера" << endl;
+    SeqContainer seq;
+    for (int i = 0; i < 10; ++i) seq.push_back(i);
+    seq.print();
+    cout << "Размер: " << seq.size() << endl;
+
+    seq.erase(2);
+    seq.erase(3);
+    seq.erase(4);
+    seq.print();
+
+    seq.insert(0, 10);
+    seq.print();
+
+    seq.insert(seq.size() / 2, 20);
+    seq.print();
+
+    seq.push_back(30);
+    seq.print();
 
     return 0;
 }
