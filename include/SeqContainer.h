f043f6fd51a107eba14876823cfb33ff5e99d00d
van06-0419 <zhiyuan.wang@yandex.ru>
2025-11-16 14:50:52 +0700

    Update SeqContainer.h

--- include/SeqContainer.h
+++ include/SeqContainer.h
@@ -1,114 +1,116 @@
 #pragma once
 #include <iostream>
 #include <stdexcept>
-#include <utility>
+using namespace std;
 
+template<typename T>
 class SeqContainer {
 private:
-	int* data;
-	size_t m_size;
-	size_t m_capacity;
+    T* data;
+    size_t size;
+    size_t capacity;
 
-	void reallocate(size_t new_capacity) {
-		int* new_data = new int[new_capacity];
-		for (size_t i = 0; i < m_size; i++) 
-			new_data[i] = std::move(data[i]);
-		delete[] data;
-		data = new_data;
-		m_capacity = new_capacity;
+	void ensure_capacity() {
+		if (size == capacity) {
+			size_t new_cap;
+			if (capacity == 0) new_cap = 1;
+			else new_cap = (size_t)(capacity * 1.5);
+
+			T* new_data = new T[new_cap];
+			for (size_t i = 0; i < size; ++i)
+				new_data[i] = std::move(data[i]);
+			delete[] data;
+			data = new_data;
+			capacity = new_cap;
+		}
 	}
+
+    void clear() noexcept {
+        data = nullptr;
+        size = 0;
+        capacity = 0;
+    }
 
 public:
-	SeqContainer() : data(nullptr),m_size(0),m_capacity(0) {}
-	~SeqContainer() { delete[] data; }
+    SeqContainer() : data(nullptr), size(0), capacity(0) {}
+    ~SeqContainer() { delete[] data; }
 
-	SeqContainer(SeqContainer&& other) noexcept
-		: data(other.data), m_size(other.m_size), m_capacity(other.m_capacity) {
-		other.data = nullptr;
-		other.m_size = 0;
-		other.m_capacity = 0;
-	}
+    SeqContainer(const SeqContainer&) = delete;
+    SeqContainer& operator=(const SeqContainer&) = delete;
 
-	SeqContainer& operator=(SeqContainer&& other) noexcept {
-		if (this != &other) {
-			delete[] data;
-			data = other.data;
-			m_size = other.m_size;
-			m_capacity = other.m_capacity;
-			other.data = nullptr;
-			other.m_size = 0;
-			other.m_capacity = 0;
-		}
-		return *this;
-	}
+    SeqContainer(SeqContainer&& other) noexcept
+        : data(std::move(other.data))
+        , size(std::move(other.size))
+        , capacity(std::move(other.capacity))
+    {
+        other.clear();
+    }
 
-	void push_back(const int& value) {
-		if (m_size >= m_capacity) {
-			size_t new_capacity;
-			if (m_capacity == 0) new_capacity = 2;
-			else new_capacity = static_cast<size_t>(m_capacity * 1.5);
-			reallocate(new_capacity);
-		}
-		data[m_size++] = value;
-	}
+    SeqContainer& operator=(SeqContainer&& other) noexcept {
+        if (this == &other) return *this;
 
-	void insert(size_t pos, const int& value) {
-		if (pos > m_size)
-			throw std::out_of_range("outside");
+        delete[] data;
+        data     = std::move(other.data);
+        size     = std::move(other.size);
+        capacity = std::move(other.capacity);
+        other.clear();
 
-		if (m_size >= m_capacity) {
-			size_t new_capacity;
-			if (m_capacity == 0) new_capacity = 2;
-			else new_capacity = static_cast<size_t>(m_capacity * 1.5);
-			reallocate(new_capacity);
-		}
+        return *this;
+    }
 
-		for (size_t i = m_size; i > pos; i--) data[i] = std::move(data[i - 1]);
+    void push_back(const T& value) {
+        ensure_capacity();
+        data[size] = value;
+        ++size;
+    }
 
-		data[pos] = value;
-		++m_size;
-	}
+    void insert(size_t index, const T& value) {
+        if (index > size) throw out_of_range("Insert index out of range");
+        ensure_capacity();
 
-	void erase(size_t pos) {
-		if (pos >= m_size)
-			throw std::out_of_range("outside");
+        for (size_t i = size; i > index; --i)
+            data[i] = std::move(data[i - 1]);
+        data[index] = value;
+        ++size;
+    }
 
-		for (size_t i = pos; i < m_size - 1; i++)
-			data[i] = std::move(data[i + 1]);
-		--m_size;
-	}
+    void erase(size_t index) {
+        if (index >= size) throw out_of_range("Erase index out of range");
 
-	size_t size() const { return m_size; }
+        for (size_t i = index; i < size - 1; ++i)
+            data[i] = std::move(data[i + 1]);
+        --size;
+    }
 
-	int& operator[] (size_t index) {
-		if (index >= m_size)
-			throw std::out_of_range("outside");
-		return data[index];
-	}
+    size_t get_size() const { return size; }
 
-	const int& operator[](size_t index) const {
-		if (index >= m_size)
-			throw std::out_of_range("outside");
-		return data[index];
-	}
+    T& operator[](size_t index) {
+        if (index >= size) throw out_of_range("Index out of range");
+        return data[index];
+    }
 
-	void print() const {
-		for (size_t i = 0; i < m_size; ++i) {
-			std::cout << data[i];
-			if (i < m_size - 1) std::cout << ", ";
-			else std::cout << "\n";
-		}
-	}
+    const T& operator[](size_t index) const {
+        if (index >= size) throw out_of_range("Index out of range");
+        return data[index];
+    }
 
-	class Iterator {
-		int* ptr;
-	public:
-		Iterator(int* p) : ptr(p) {}
-		int& operator*() const { return *ptr; }
-		Iterator& operator++() { ++ptr; return *this; }
-		bool operator!=(const Iterator& other) const { return ptr != other.ptr; }
-	};
+    void print() const {
+        for (size_t i = 0; i < size; ++i) {
+            cout << data[i];
+            if (i < size - 1) cout << ", ";
+        }
+        cout << endl;
+    }
 
-	Iterator begin() const { return Iterator(data); }
-	Iterator end() const { return Iterator(data + m_size); }
+    class iterator {
+        T* ptr;
+    public:
+        iterator(T* p) : ptr(p) {}
+        T& operator*() { return *ptr; }
+        iterator& operator++() { ++ptr; return *this; }
+        bool operator!=(const iterator& other) const { return ptr != other.ptr; }
+    };
+
+    iterator begin() { return iterator(data); }
+    iterator end() { return iterator(data + size); }
 };
