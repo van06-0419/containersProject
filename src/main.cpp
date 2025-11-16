ce16af0b9492d94655c92b71bcf3b26e4e2d3297
van06-0419 <zhiyuan.wang@yandex.ru>
2025-11-16 14:59:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     cout << endl << "Тест односвязного списка" << endl;
-    SinglyListContainer slist;
+    SinglyListContainer<int> slist;
     for (int i = 0; i < 10; ++i) slist.push_back(i);
     slist.print();
     cout << "Размер: " << slist.size() << endl;
@@ -15,18 +15,18 @@ int main() {
     slist.erase(3);
     slist.erase(4);
     slist.print();
-    
+
     slist.insert(0, 10);
     slist.print();
-    
+
     slist.insert(slist.size() / 2, 20);
     slist.print();
-    
+
     slist.push_back(30);
     slist.print();
 
     cout << endl << "Тест двусвязного списка" << endl;
-    ListContainer list;
+    ListContainer<int> list;
     for (int i = 0; i < 10; ++i) list.push_back(i);
     list.print();
     cout << "Размер: " << list.size() << endl;
@@ -46,10 +46,10 @@ int main() {
     list.print();
 
     cout << "Тест последовательного контейнера" << endl;
-    SeqContainer seq;
+    SeqContainer<int> seq;
     for (int i = 0; i < 10; ++i) seq.push_back(i);
     seq.print();
-    cout << "Размер: " << seq.size() << endl;
+    cout << "Размер: " << seq.get_size() << endl;
 
     seq.erase(2);
     seq.erase(3);
@@ -59,7 +59,7 @@ int main() {
     seq.insert(0, 10);
     seq.print();
 
-    seq.insert(seq.size() / 2, 20);
+    seq.insert(seq.get_size() / 2, 20);
     seq.print();
 
     seq.push_back(30);
